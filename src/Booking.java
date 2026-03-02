165a0ce41c0bc039f39769b3718d90ac55bf9e7a
anshe <anshengineer1873@gmail.com>
2026-03-02 16:38:03 -0500

    implement booking

---
+++ src/Booking.java
@@ -0,0 +1,61 @@
+public class Booking {
+    private String eventID;
+    private String bookingID;
+    private String userID;
+    private String createdAt;
+    public enum BookingStatus { Confirmed, Waitlisted, Cancelled }
+    private BookingStatus bookingStatus;
+    private int NumOfBookings;
+
+    public String getEventID() {
+        return eventID;
+    }
+
+    public String getBookingID() {
+        return bookingID;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public BookingStatus getBookingStatus() {
+        return bookingStatus;
+    }
+
+    public void setEventID(String eventID) {
+        this.eventID = eventID;
+    }
+
+    public void setBookingID(String bookingID) {
+        this.bookingID = bookingID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public void setBookingStatus(BookingStatus bookingStatus) {
+        this.bookingStatus = bookingStatus;
+    }
+
+    public Booking(){
+
+    }
+
+    public Booking(String bookingID, String userID, String eventID, String createdAt, BookingStatus bookingStatus){
+        this.bookingID = bookingID;
+        this.userID = userID;
+        this.eventID = eventID;
+        this.createdAt = createdAt;
+        this.bookingStatus = bookingStatus;
+    }
+}
---
+++ src/ManageBooking.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class ManageBooking {
+    private ArrayList<Booking> bookings;
+
+    public ManageBooking(ArrayList<Booking> bookings) {
+        bookings = new ArrayList<>();
+    }
+
+    public boolean createBooking(Booking newBooking){
+        for (Booking booking : this.bookings) {
+            if (booking.getBookingID().equalsIgnoreCase(newBooking.getBookingID())
+                    && booking.getEventID().equalsIgnoreCase(newBooking.getEventID())) {
+                return false;
+            }
+        }
+
+        this.bookings.add(newBooking);
+        return true;
+    }
+
+    public
+
+    public void bookingCancel(){
+        //  this.bookingStatus = BookingStatus.Cancelled;
+    }
+
+    public void bookingView(){
+
+    }
+}
