b2360150f520853ef5984087e6731bba64f2462b
Louis Nguyen <louis.nguyen550@gmail.com>
2026-03-17 20:37:31 -0400

    implement toString method

--- src/main/java/Concert.java
+++ src/main/java/Concert.java
@@ -3,24 +3,34 @@ import java.time.LocalDateTime;
 public class Concert extends Event {
     private String ageRestriction;
 
-//    constructor
+    //    no-arg constructor
+    public Concert() {
+        this.ageRestriction = "";
+    }
+
+    //    full constructor
     public Concert(String eventId, String title, LocalDateTime dateTime, String location,
-                   int capacity, Event.Status status, String ageRestriction) {
+                   int capacity, Status status, String ageRestriction) {
         super(eventId, title, dateTime, location, capacity, status);
         setAgeRestriction(ageRestriction);
     }
 
-//    setter
+    //    setter
     public void setAgeRestriction(String ageRestriction) {
         this.ageRestriction = ageRestriction;
     }
 
-//    getter
+    //    getter
     public String getAgeRestriction() {
         return this.ageRestriction;
     }
 
     public void updateTypeSpecificField(String ageRestriction) {
         setAgeRestriction(ageRestriction);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Type: Concert, Age Restriction: " + ageRestriction;
     }
 }
